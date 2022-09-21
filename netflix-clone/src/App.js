da1b5d110c1278136cc3a58fb7bbd0913f089b84
moatassim <moatassimbarakat@gmail.com>
2022-09-21 01:01:01 +0100

    netflixClone  all

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Row from './Row';
+import './App.css';
+import requests from './requests';
+import Banner from './Banner.js'
+import Nav from './Nav.js'
+
+function App() {
+  return (
+   <div className="App">
+   <Nav/>
+   <Banner/>
+   <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+   <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+   <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+   <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+   <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+   <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+   <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+   <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+   </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/Nav.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <div className='nav'></div>
+  )
+}
